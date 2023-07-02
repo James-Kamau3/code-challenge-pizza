1886b552cc6aaa16ead206060e3e875a30b638e8
James <kamaujames3705@gmail.com>
2023-07-02 19:39:10 +0300

    Added Restaurant class

--- app/models.py
+++ app/models.py
@@ -6,5 +6,9 @@ class Restaurant(db.Model):
     __tablename__ = 'Restaurant'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
 
 # add any models you may need. 
+
+
