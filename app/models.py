577b0525abd0dc16d07bf73101aa8612c586d35d
James <kamaujames3705@gmail.com>
2023-07-02 19:44:57 +0300

    Added RestaurantPizza class

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Restaurant(db.Model):
-    __tablename__ = 'Restaurant'
+    __tablename__ = 'restaurants'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
@@ -13,7 +13,7 @@ class Restaurant(db.Model):
 
 class Pizza(db.Model):
 
-    __tablename__ = 'Pizza'
+    __tablename__ = 'pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
@@ -22,4 +22,14 @@ class Pizza(db.Model):
     created_at = db.Column(db.DateTime, server_default = db.func.now())
     updated_at = db.Column(db.DateTime, onupdate = db.func.now())
 
+class RestaurantPizza(db.Model):
 
+    __tablename__ = 'restaurant_pizzas'
+
+    id = db.Column(db.Integer, primary_key = True)
+    pizza_id = db.Column(db.Integer)
+    restaurant_id = db.Column(db.Integer)
+    price = db.Column(db.Integer)
+    
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
